60cd795333e8d4eedec48f3c467bc01458e3be42
SREEJITH RAJ <sreejith61206@gmail.com>
2026-01-15 08:17:49 +0530

    map

--- frontend/src/components/HawkinsMap.jsx
+++ frontend/src/components/HawkinsMap.jsx
@@ -1,56 +1,22 @@
 import { useEffect } from "react";
 import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet.heat";
 
 export default function HawkinsMap(){
 
   useEffect(()=>{
 
-    const map = L.map("hawkins-map").setView([28.61,77.21],13);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-
-    const zones = {
-      North:[28.62,77.22],
-      South:[28.6,77.21],
-      East:[28.61,77.24],
-      West:[28.61,77.19]
-    };
-
-    const stories = [
-      {zone:"North", intensity:4},
-      {zone:"North", intensity:5},
-      {zone:"South", intensity:3},
-      {zone:"East", intensity:4},
-      {zone:"East", intensity:5}
-    ];
-
-    function blur(p){
-      return [
-        p[0] + (Math.random()-0.5)*0.01,
-        p[1] + (Math.random()-0.5)*0.01
-      ];
-    }
-
-    const heatData = [];
-
-    stories.forEach(s=>{
-      const pos = blur(zones[s.zone]);
-      heatData.push([pos[0],pos[1],s.intensity]);
+    const map = L.map("hawkins-map", {
+      crs: L.CRS.Simple,
+      minZoom: -1
     });
 
-    L.heatLayer(heatData,{
-      radius:30,
-      blur:25,
-      minOpacity:0.4
-    }).addTo(map);
+    const bounds = [[0,0],[1000,1000]]; // match your SVG size
 
-    return () => {
-      map.remove();
-    };
+    L.imageOverlay("/maps/hawkins (1).svg", bounds).addTo(map);
+
+    map.fitBounds(bounds);
 
   },[]);
 
-  return <div id="hawkins-map" style={{height:"500px", width:"100%"}}></div>;
+  return <div id="hawkins-map" style={{height:"500px"}}></div>;
 }
